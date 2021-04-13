d08939f207d0be641ec1cad07705f28a3290932e
ioaneduardmarin <58357224+ioaneduardmarin@users.noreply.github.com>
2021-04-13 14:46:05 +0300

    first commit

---
+++ src/views/ExperienceDetails.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <section>
+      <h2>{{ experience.name }}</h2>
+      <div class="experience-details">
+        <img
+          :src="require(`@/assets/${experience.image}`)"
+          :alt="experience.name"
+        />
+        <p>{{ experience.description }}</p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import store from "@/store.js";
+export default {
+  props: {
+    slug: {
+      type: String,
+      required: true,
+    },
+    experienceSlug: {
+      type: String,
+      reuqired: true,
+    },
+  },
+  computed: {
+    destination() {
+      return store.destinations.find(
+        (destination) => destination.slug === this.slug
+      );
+    },
+    experience() {
+      return this.destination.experiences.find(
+        (experience) => experience.slug === this.experienceSlug
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+.experience-details {
+  display: flex;
+  justify-content: space-between;
+  padding: 40px 0;
+}
+
+p {
+  margin: 0 40px;
+  font-size: 20 px;
+  text-align: left;
+}
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Welcome {{ user }}</h1>
+    <button @click="logOut" class="btn">Log out</button>
+    <p>
+      <router-link
+        :to="{
+          name: 'invoices',
+        }"
+        >Invoices</router-link
+      >
+    </p>
+  </div>
+</template>
+
+<script>
+import store from "@/store.js";
+
+export default {
+  data() {
+    return {
+      user: store.user,
+    };
+  },
+  methods: {
+    logOut() {
+      store.user = null;
+      this.$router.push("/");
+    },
+  },
+};
+</script>
